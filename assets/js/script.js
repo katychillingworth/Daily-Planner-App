507401ddae42b6e3202c881f73c9956a6093454e
katychillingworth <katy.chillingworth@hotmail.com>
2023-01-27 19:11:33 +0000

    adds date to header

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// TODO Add current date to the header
+var currentDay = moment();
+$("#currentDay").text(currentDay.format("MMMM Do YYYY"));
+
+// TODO Add rows with a for loop to represent timeblocks
+
+// TODO Add colours for past/present/future
+
+// TODO Allow user to enter an event when they click timeblock
+
+// TODO Save the event in local storage
+
+// TODO Persist events between page refreshes 
+
+// TODO Add a button to clear calendar
+
+// TODO 
