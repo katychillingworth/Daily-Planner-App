27857a2affca391e3857bf06526b6fb7df26b825
katychillingworth <katy.chillingworth@hotmail.com>
2023-02-01 20:03:09 +0000

    adds local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,23 @@ var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMMM Do YYYY"));
 
 // TODO Add timeblocks for standard business hours when the user scrolls down
+// Is in HTML file
 
 // TODO Add colours for past/present/future
 
 // TODO Allow user to enter an event when they click timeblock
+// Is in HTML file
 
 // TODO Save the event in local storage
+$(".saveBtn").on("click", function () {
 
-// TODO Persist events between page refreshes 
+    var eventAt9 = $("#9").val();
+    localStorage.setItem('9am:', eventAt9);
+})
+
+// TODO Persist events between page refreshes
+
+function loadCalendar() {}
 
 // TODO Add a button to clear calendar
 
